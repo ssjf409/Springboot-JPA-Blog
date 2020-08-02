590159b8a24072abd6033630429b1a42f166cadc
JangDonghyeok <ssjf409@naver.com>
2020-08-03 01:16:07 +0900

    make User, Board, Reply

---
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -0,0 +1,72 @@
+package com.cos.blog.test;
+
+
+import java.util.function.Supplier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cos.blog.model.RoleType;
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+
+
+// html파일이 아니라 data를 리턴해주는 controller =RestController
+@RestController
+public class DummyControllerTest {
+	
+	@Autowired // 의존성 주입(DI)
+	private UserRepository userRepository;
+	
+	// {id} 주소로 파마리터를 전달 받을 수 있음.
+	// http://localhost:8000/blog/dummy/user/3
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		// user/4을 찾으면 내가 데이터베이스에서 못 찾아오게 되면 user가 null이 될 것 아냐?
+		// 그럼 return null 이 리턴이 되잖아.. 그럼 프로그램에 문제가 있지 않겠니?
+		// Optional로 너의 User객체를 감싸서 가져올테니 null인지 아닌지 판단해서 return 해!
+		
+// 		람다식
+//		User user =  userRepository.findById(id).orElseThrow(() -> {
+//			return new IllegalArgumentException("해당 사용자는 없습니다.");
+//		});
+//		return user;
+//	}
+		
+		User user =  userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				// TODO Auto-generated method stub
+				return new IllegalArgumentException("해당 사영자가 없습니다");
+			}
+		});
+		// 요청 : 웹브라우저
+		// user 객체 = 자바오브젝트
+		// 변환 (웹브라우저가 이해할 수 있는 데이터) -> json (Gson 라이브러리)
+		// 스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
+		// 만약에 자바 ㅇ브젝트를 리턴하면 MessageConverter가 Jackson 라이브러리를 호출해서
+		// user 오브젝트를 json으로 변환해서 브라우저에게 던져줍니다. 
+		return user;
+	}
+	
+	
+	// http://localhost:8000/blog/dummy/join (요청)
+	// http의 body에 username, password, email 데이터를 가지고 (요청)
+	@PostMapping("/dummy/join")
+	public String join(User user) { // key = value (약속된 규칙)
+		System.out.println("id : " + user.getId());
+		System.out.println("username : " + user.getUsername());
+		System.out.println("password : " + user.getPassword());
+		System.out.println("email : " + user.getEmail());
+		System.out.println("role : " + user.getRole());
+		System.out.println("createData : " + user.getCreateData());
+		
+		user.setRole(RoleType.USER);
+		userRepository.save(user);
+		return "회원가입이 완료되었습니다.";
+	}
+}
